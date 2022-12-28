0a72c0efbb77e6f768b710b7da5ad745e525d2c1
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-28 09:29:22 +0400

    version update

--- PowerCollection/build_and_publish_nuget.cmd
+++ PowerCollection/build_and_publish_nuget.cmd
@@ -1,3 +1,3 @@
 dotnet pack --configuration Release
 
-dotnet nuget push "./bin/Release/Cthulhu-tech.PowerCollections.0.0.2-beta.nupkg" --source github --api-key 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
+dotnet nuget push "./bin/Release/Cthulhu-tech.PowerCollections.0.0.3-beta.nupkg" --source github --api-key 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
